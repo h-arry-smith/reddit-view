40324d198fdfbdcd05a3336eb7a66890681bdc2e
Harry Smith <harry@hsmith.net>
2020-12-20 14:29:28 +0000

    Header tailwind styling

--- src/app/NavBar.js
+++ src/app/NavBar.js
@@ -3,9 +3,13 @@ import {Link} from 'react-router-dom'
 
 export const NavBar = () => {
   return (
-    <div className="NavBar">
-      <h1 className="NavBar-title">RedditView</h1>
-      <Link to="/">Home</Link>
+    <div className="flex items-center justify-between w-auto h-20 px-4 text-center bg-white border-b-4 border-yellow-500">
+      <div className="">
+        <h1 className="text-3xl font-semibold">RedditView</h1>
+      </div>
+      <div className="text-lg font-semibold hover:text-yellow-600">
+        <Link to="/">Home</Link>
+      </div>
     </div>
   );
 }
