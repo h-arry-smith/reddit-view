0ad5613ccd267a5ce47541e92e5829ecca1c1cc4
Harry Smith <harry@hsmith.net>
2020-12-21 13:25:15 +0000

    CommentList component tests

--- src/features/comments/CommentList.js
+++ src/features/comments/CommentList.js
@@ -2,6 +2,10 @@ import React from 'react'
 import { Comment } from './Comment'
 
 export const CommentList = ({comments}) => {
+  if (!comments) {
+    return <div>No Comments</div>
+  }
+
   return (
     <div className="">
       {comments.map(comment => {
