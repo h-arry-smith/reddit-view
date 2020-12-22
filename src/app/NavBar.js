fb50dd87c0bebfe2e723a3cde412d43e5094a413
Harry Smith <harry@hsmith.net>
2020-12-22 12:41:27 +0000

    navbar responds nice when resizing

--- src/app/NavBar.js
+++ src/app/NavBar.js
@@ -6,14 +6,14 @@ import { SubredditSelector } from '../features/subreddit/SubredditSelector'
 export const NavBar = () => {
   return (
     <div className="px-4 bg-white border-b-4 border-yellow-500">
-      <div className="flex items-center justify-between w-auto mt-4 text-center">
+      <div className="flex items-center justify-between w-full mt-4 text-center sm:justify-start">
         <div className="">
           <h1 className="text-3xl font-semibold">RedditView</h1>
         </div>
-        <div>
+        <div className="mt-1 ml-4">
           <SubredditSelector />
         </div>
-        <div className="text-lg font-semibold hover:text-yellow-600">
+        <div className="w-full text-lg font-semibold text-right hover:text-yellow-600 sm:justify-self-end">
           <Link to="/">
             Home
           </Link>
